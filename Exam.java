e5ee8908ef02002a661f9265e954612c7481ec1b
s13012 <s13012@std.it-college.ac.jp>
2015-11-04 11:44:14 +0900

    問13完了

--- Exam.java
+++ Exam.java
@@ -150,7 +150,15 @@ public class Exam {
 	*/
 
 	String FizzBuzz(int x) {
-		return sDummy;
+		if (x % 15 == 0) {
+			return "FizzBuzz";
+		} else if (x % 3 == 0) {
+			return "Fizz";
+		} else if (x % 5 == 0) {
+			return "Buzz";
+		} else {
+			return String.valueOf(x);
+		}
 	}
 
 	/*
